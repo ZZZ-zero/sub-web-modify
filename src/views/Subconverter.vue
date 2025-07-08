15dda35e51e2d35c594e432f719f38f5263b7c12
zero <30883525+ZZZ-zero@users.noreply.github.com>
2025-07-09 02:32:45 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -332,7 +332,7 @@ export default {
           "自动判断客户端": "auto",
         },
         shortTypes: {
-          "v1.mk": "https://links.1308892875.workers.dev/99loli",
+          "99loli": "https://links.1308892875.workers.dev/99loli",
           "v1.mk": "https://v1.mk/short",
           "d1.mk": "https://d1.mk/short",
           "dlj.tf": "https://dlj.tf/short",
