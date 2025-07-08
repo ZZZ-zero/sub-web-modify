2f47b304c4fb6f623daee1b7dd20d24a313758f4
zero <30883525+ZZZ-zero@users.noreply.github.com>
2025-07-09 03:38:32 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -787,7 +787,7 @@ export default {
         sourceSubUrl: "",
         clientType: "",
         customBackend: this.getUrlParam() == "" ? defaultBackend : this.getUrlParam(),
-        shortType: "https://links.1308892875.workers.dev",
+        shortType: "https://links.1308892875.workers.dev/short",
         remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",
         excludeRemarks: "",
         includeRemarks: "",
